6ce8999309524e9e77683c0dada1224af1d79ffc
Stefanomerigo <145322600+Stefanomerigo@users.noreply.github.com>
2025-10-23 09:33:16 +0000

    link1.1

--- Es_anagrafica/script.js
+++ Es_anagrafica/script.js
@@ -3,7 +3,8 @@ function Tabella() {
 
     let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "dati.json", true);
+
+    xhr.open("GET", "Es_anagrafica/dati.json", true);
     xhr.send();
 
     xhr.onload = function () {
@@ -50,7 +51,7 @@ function Tabella() {
 function Maggiorenni() {
     let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "dati.json", true);
+    xhr.open("GET", "Es_anagrafica/dati.json", true);
     xhr.send();
 
     xhr.onload = function () {
@@ -101,7 +102,7 @@ function Maggiorenni() {
 function RicercaLettera() {
     let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "dati.json", true);
+    xhr.open("GET", "Es_anagrafica/dati.json", true);
     xhr.send();
 
     xhr.onload = function () {
@@ -153,7 +154,7 @@ function RicercaLettera() {
 function RicercaGenerazione() {
     let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "dati.json", true);
+    xhr.open("GET", "Es_anagrafica/dati.json", true);
     xhr.send();
 
     xhr.onload = function () {
